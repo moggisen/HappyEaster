cce022e44a01dd6d5db8c2aa1decc4f1516d9337
moggisen <mathilda.tidbeck@gmail.com>
2025-04-17 09:50:18 +0200

    Questions

--- HappyEaster/src/data/questions.js
+++ HappyEaster/src/data/questions.js
@@ -1,7 +1,34 @@
 export const questions = [
-    {
-        question: "Vad sympoliserar ägget under påsken?",
-        options: ["En dinosaurie","Liv och återfödelse",]
-        
-    }
-]
+  {
+    question: "Vad sympoliserar ägget under påsken?",
+    options: [
+      "En dinosaurie",
+      "Liv och återfödelse",
+      "Påskharen",
+      "Vinterns slut",
+    ],
+    answer: "Liv och återfödelse",
+  },
+  {
+    question: "Vilken färg förknippas mest med påsken i Sverige?",
+    options: ["Röd", "Gul", "Blå", "Grön"],
+    answer: "Gul",
+  },
+  {
+    question: "Vad är en klassisk svensk påskrätt?",
+    options: ["Tacos", "Sill och ägg", "Pizza", "Pasta Carbonara"],
+    answer: "Sill och ägg",
+  },
+  {
+    question: "Vilket djur delat ut påskägg enligt traditionen?",
+    options: ["Påskkatten", "Påskankan", "Påskharen", "Påsktuppen"],
+    answer: "Påskharen",
+  },
+  {
+    question: "Vad brukar barn klä ut sig till under påsken?",
+    options: ["Julgranar", "Påskgummor", "Superhjältar", "Snögubbar"],
+    answer: "Påskgummor",
+  },
+];
+
+export default questions;

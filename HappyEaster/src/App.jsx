3aa94cd0a4abbc64a980f89b1d37498a5fcb95aa
moggisen <mathilda.tidbeck@gmail.com>
2025-04-17 10:26:11 +0200

    app.jsx

--- HappyEaster/src/App.jsx
+++ HappyEaster/src/App.jsx
@@ -1,13 +1,31 @@
-
 import "./styles/quiz.css";
+import { useQuiz } from "./hooks/useQuiz";
+import Question from "./components/Questions";
+import Result from "./components/Result";
 
 function App() {
-  
+const {
+  currentQuestion, 
+  score,
+  isFinished,
+  totalQuestions,
+  handleAnswer,
+  restart 
+}  = useQuiz();
 
   return (
-    <>
-    <h1>Hello!</h1>
-    </>
+   <div className="quiz-container">
+    <h1>🐣 PåskQuiz 🐣</h1>
+    {isFinished} ? (
+      <Result score={score} total={totalQuestions} onRestart={restart} />
+    ) : (
+      <Question 
+      question={currentQuestion.question}
+      options={currentQuestion.options}
+      onAnswer={handleAnswer}
+      />
+    )
+   </div>
   );
 }
 

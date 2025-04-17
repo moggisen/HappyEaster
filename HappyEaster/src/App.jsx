385f19a151e755068e864de00283c6a6d65feece
moggisen <mathilda.tidbeck@gmail.com>
2025-04-17 09:28:30 +0200

    upload files

---
+++ HappyEaster/src/App.jsx
@@ -0,0 +1,14 @@
+
+import "./styles/quiz.css";
+
+function App() {
+  
+
+  return (
+    <>
+    <h1>Hello!</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ HappyEaster/src/data/questions.js
@@ -0,0 +1,7 @@
+export const questions = [
+    {
+        question: "Vad sympoliserar ägget under påsken?",
+        options: ["En dinosaurie","Liv och återfödelse",]
+        
+    }
+]
